8c2500ad1a0228c02b367f744e08248c5bee76e8
cont1n3nt <type.twitch@gmail.com>
2026-02-06 17:39:43 +0300

    Additional model training. New confusion matrix and word contribution graphs

--- src/predict.py
+++ src/predict.py
@@ -8,7 +8,7 @@ def predict_email(text: str):
     return pred, proba
 
 if __name__ == "__main__":
-    test_email = "Verify your account now by clicking the link"
+    test_email = "Your account has been suspended. Click here."
     pred, proba = predict_email(test_email)
     
     print("Prediction:", pred)
